02a33b1111899b645a2f3973a713002027f86d07
XaHDpE <xahdpe@gmail.com>
2019-08-07 18:36:16 +0300

    carousel related changes.

--- public/js/client_functions.js
+++ public/js/client_functions.js
@@ -16,7 +16,10 @@ $( ()=> {
   $('#sortable, #active_pl').disableSelection();
 });
 */
-$(() => {
+$(
+  /*
+  () => {
+
 
     Sortable.create(avaialableVidsList,
       {
@@ -54,6 +57,32 @@ $(() => {
       });
 
   },
+   */
 );
 // Simple list
 
+/*
+    Carousel
+*/
+$('#carousel-example').on('slide.bs.carousel', function (e) {
+  /*
+      CC 2.0 License Iatek LLC 2018 - Attribution required
+  */
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 5;
+  var totalItems = $('.carousel-item').length;
+
+  if (idx >= totalItems-(itemsPerSlide-1)) {
+    var it = itemsPerSlide - (totalItems - idx);
+    for (var i=0; i<it; i++) {
+      // append slides to end
+      if (e.direction=="left") {
+        $('.carousel-item').eq(i).appendTo('.carousel-inner');
+      }
+      else {
+        $('.carousel-item').eq(0).appendTo('.carousel-inner');
+      }
+    }
+  }
+});
