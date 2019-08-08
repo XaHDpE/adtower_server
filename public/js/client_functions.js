bed3e4d55e15b5fb811845e0edb47ba1f5954dd9
Andrey Isachkov <xahdpe@gmail.com>
2019-08-08 12:52:54 +0300

    caruosel to horziontal list change

--- public/js/client_functions.js
+++ public/js/client_functions.js
@@ -1,23 +1,4 @@
-/*
-$( ()=> {
-  $("#active_pl, #sortable").children().uniqueId().end().sortable({
-    connectWith: "ul",
-    update: function (event, ui) {
-      var data = $(this).sortable('serialize');
-      console.log(data);
-      $.ajax({
-        data: data,
-        type: 'POST',
-        url: '/your/url/here'
-      });
-    }
-  });
-
-  $('#sortable, #active_pl').disableSelection();
-});
-*/
 $(
-  /*
   () => {
 
 
@@ -57,7 +38,6 @@ $(
       });
 
   },
-   */
 );
 // Simple list
 
